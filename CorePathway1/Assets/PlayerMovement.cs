b8f453955a2d75a8e0a838fb5e8a898365a7086b
Marcus Moreno <marcusmoreno@Marcuss-MacBook-Air.local>
2023-01-05 13:20:08 -0800

    Setting up

---
+++ CorePathway1/Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public float speed = 6f;
+
+    public void characterMovement(){
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)(//if we are moving
+            float turnSmoothTime = 0.1f;
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf
+        )    
+    }
+}
