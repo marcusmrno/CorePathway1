3e67b304e1ca5be2f769526908a80481e742332a
Marcus Moreno <marcusmoreno@Marcuss-MacBook-Air.local>
2023-01-09 12:12:31 -0800

    New Updates n shit
    worked on player control
       -mouse movement

--- CorePathway1/Assets/PlayerMovement.cs
+++ CorePathway1/Assets/PlayerMovement.cs
@@ -8,17 +8,20 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        characterMovement();
+        cameraControl();
     }
     
     public Transform cam;
     public float speed = 6f;
+    float turnSmoothVelocity;
+    public CharacterController controller;
 
     public void characterMovement(){
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -30,6 +33,25 @@ public class PlayerMovement : MonoBehaviour
             float turnSmoothTime = 0.1f;
 
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
+    }
+
+
+    [SerializeField] private float sens = 1;
+    [SerializeField] private bool invertVertical;
+
+    public void cameraControl()
+    {
+        var deltaMouse = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        Vector2 deltaRotation = deltaMouse * sens;
+        deltaRotation.y *= invertVertical ? 1.0f : -1.0f;
+
+        transform.Rotate(Vector3.right, deltaRotation.y);
+        transform.Rotate(Vector3.up, deltaRotation.x);
     }
 }
